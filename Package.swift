7d9723e8fffcc4c022e4b8128ac21f2813edb10b
liuhongli <>
2024-12-05 10:38:27 +0800

    [t0]依赖库修改为gitee源

--- Package.swift
+++ Package.swift
@@ -13,16 +13,29 @@ let package = Package(
             targets: ["BearSdk"]
         )
     ],
+//    dependencies: [
+//        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.2")),
+//        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.3")),
+//        .package(url: "https://gitee.com/cellgit/Result.git", from: "5.0.0")
+////        .package(url: "https://github.com/antitypical/Result.git", from: "5.0.0")
+//    ],
+    
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.2")),
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.3")),
+        
+        .package(url: "https://gitee.com/cellgit/RxSwift.git", .upToNextMajor(from: "6.8.0")),
+        .package(url: "https://gitee.com/cellgit/ReactiveSwift.git", .upToNextMajor(from: "6.7.0")),
+        .package(url: "https://gitee.com/cellgit/Alamofire.git", .upToNextMajor(from: "5.10.2")),
+        .package(url: "https://gitee.com/cellgit/Moya.git", .upToNextMajor(from: "15.0.3")),
         .package(url: "https://gitee.com/cellgit/Result.git", from: "5.0.0")
+        
 //        .package(url: "https://github.com/antitypical/Result.git", from: "5.0.0")
     ],
+    
+    
     targets: [
         .target(
             name: "BearSdk",
-            dependencies: ["Alamofire", "Moya", "Result"]
+            dependencies: ["Alamofire", "Moya", "Result", "RxSwift", "ReactiveSwift"]
         ),
         .testTarget(
             name: "BearSdkTests",
